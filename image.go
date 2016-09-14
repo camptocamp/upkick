f467a6200f64dfea20eecb1df10c845779c5a53d
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-09-14 17:08:08 +0200

    Use log

--- image.go
+++ image.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"time"
+
+	log "github.com/Sirupsen/logrus"
 
 	"github.com/docker/engine-api/types"
 	"github.com/pkg/errors"
@@ -22,7 +23,7 @@ type imageHash struct {
 
 // getImages returns a slice of image
 func (h *handler) getImages() (images map[string]*image, err error) {
-	fmt.Println("Getting images")
+	log.Debug("Getting images")
 	containers, err := h.Client.ContainerList(context.Background(), types.ContainerListOptions{})
 	if err != nil {
 		err = errors.Wrap(err, "failed to list containers")
@@ -48,7 +49,7 @@ func (h *handler) getImages() (images map[string]*image, err error) {
 			i.hashes[c.ImageID] = &imageHash{}
 			h = i.hashes[c.ImageID]
 		}
-		fmt.Printf("Adding %s with hash %s to %s\n", c.ID, c.ImageID, cont.Config.Image)
+		log.Debugf("Adding %s with hash %s to %s\n", c.ID, c.ImageID, cont.Config.Image)
 		h.containers = append(h.containers, c.ID)
 	}
 
@@ -62,32 +63,32 @@ func (i *image) String() string {
 
 // pull updates an image
 func (i *image) pull() error {
-	fmt.Printf("Pulling image %s\n", i)
+	log.Debugf("Pulling image %s\n", i)
 	_, _ = i.handler.Client.ImagePull(context.Background(), i.id, types.ImagePullOptions{})
 	img, _, _ := i.handler.Client.ImageInspectWithRaw(context.Background(), i.id)
 	i.hash = img.ID
-	fmt.Printf("Image %s updated to %v\n", i, i.hash)
+	log.Infof("Image %s updated to %v\n", i, i.hash)
 	return nil
 }
 
 // kick stops and removes all containers
 // using an obsolete version of the image
 func (i *image) kick() error {
-	fmt.Printf("Kicking containers for image %s\n", i)
+	log.Debugf("Kicking containers for image %s\n", i)
 
 	for h, hh := range i.hashes {
 		if h == i.hash {
 			// Already up-to-date
-			fmt.Printf("Not kicking containers for up-to-date hash %s\n", h)
+			log.Debugf("Not kicking containers for up-to-date hash %s\n", h)
 			continue
 		}
 
 		for _, c := range hh.containers {
-			fmt.Printf("Kicking container %s\n", c)
+			log.Debugf("Kicking container %s\n", c)
 			timeout := 10 * time.Second
-			fmt.Printf("Stopping container %s\n", c)
+			log.Debugf("Stopping container %s\n", c)
 			_ = i.handler.Client.ContainerStop(context.Background(), c, &timeout)
-			fmt.Printf("Removing container %s\n", c)
+			log.Debugf("Removing container %s\n", c)
 			_ = i.handler.Client.ContainerRemove(context.Background(), c, types.ContainerRemoveOptions{})
 		}
 	}
