0a542c897128b05fcb2b7a493a02c9e184b32a57
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-09-14 18:11:16 +0200

    s/CONPLICITY/UPKICK/

--- config/config.go
+++ config/config.go
@@ -11,9 +11,9 @@ import (
 // Config stores the handler's configuration and UI interface parameters
 type Config struct {
 	Version  bool   `short:"V" long:"version" description:"Display version."`
-	Loglevel string `short:"l" long:"loglevel" description:"Set loglevel ('debug', 'info', 'warn', 'error', 'fatal', 'panic')." env:"CONPLICITY_LOG_LEVEL" default:"info"`
+	Loglevel string `short:"l" long:"loglevel" description:"Set loglevel ('debug', 'info', 'warn', 'error', 'fatal', 'panic')." env:"UPKICK_LOG_LEVEL" default:"info"`
 	Manpage  bool   `short:"m" long:"manpage" description:"Output manpage."`
-	JSON     bool   `short:"j" long:"json" description:"Log as JSON (to stderr)." env:"CONPLICITY_JSON_OUTPUT"`
+	JSON     bool   `short:"j" long:"json" description:"Log as JSON (to stderr)." env:"UPKICK_JSON_OUTPUT"`
 
 	Docker struct {
 		Endpoint string `short:"e" long:"docker-endpoint" description:"The Docker endpoint." env:"DOCKER_ENDPOINT" default:"unix:///var/run/docker.sock"`
